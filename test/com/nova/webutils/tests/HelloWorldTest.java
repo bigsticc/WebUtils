6e86a19344703de78b9d1289c9c9a3b2ea4fa1c6
supernova <supernova9987@gmail.com>
2023-01-26 19:37:38 -0500

    Created testing infrastructure

---
+++ test/com/nova/webutils/tests/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package test.com.nova.webutils.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+    public static void main(String[] args) {
+        assertTrue(true);
+        System.out.println("Hello World!");
+    }
+}
