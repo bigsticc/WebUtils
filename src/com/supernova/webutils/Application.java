f9fd2312ee4c3fbe23d8695ff83a17611ea1a3a4
Supernova <Supernova9987@gmail.com>
2023-02-12 03:34:46 -0500

    Added copyright notice

--- src/com/supernova/webutils/Application.java
+++ src/com/supernova/webutils/Application.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2023 supernova
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.supernova.webutils;
 
 import com.supernova.webutils.http.HttpRequest;
